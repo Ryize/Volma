f3383d08f972d6217497d7dbc2da6ca1eee4ec93
matvejcekasov <chekashovmatvey@gmail.com>
2023-05-08 13:02:12 +0300

    feat: Добавлено затемнение к тексту заданий

--- Assets/FPSController/Scripts/TextPrint.cs
+++ Assets/FPSController/Scripts/TextPrint.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections;
 using TMPro;
 using UnityEngine.UI;
 
@@ -8,24 +7,30 @@ public class TextPrint : MonoBehaviour
     public Text MyText;
     public TextMeshPro myText;
     public TextMeshProUGUI my_text_ui;
-    void Start()
-    {
-        
-    }
-    
     void Update()
     {
         var broom = GameObject.FindGameObjectsWithTag("Item")[2];
         var dirts = GameObject.FindGameObjectsWithTag("Dirt");
         var textTask = GameObject.FindGameObjectWithTag("Task").GetComponent<TMP_Text>();
+        var image = GameObject.FindGameObjectWithTag("menuImage").GetComponent<Image>();
         if (dirts.Length == 0)
         {
             textTask.text = "Вы справились с первым заданием!";
+            image.rectTransform.sizeDelta = new Vector2(175, 65);
+            image.rectTransform.position = new Vector2(93, 383);
             return;
         }
-        if (broom.GetComponent<TakenPosition>().isTaken)
+        else if (broom.GetComponent<TakenPosition>().isTaken)
         {
-            textTask.text = "Подойдите к грязи и вытрети её (зажав левую кнопку, и двигая курсором)";;
+            textTask.text = "Подойдите к грязи и вытрети её (зажав левую кнопку, и двигая курсором)";
+            image.rectTransform.sizeDelta = new Vector2(175, 125);
+            image.rectTransform.position = new Vector2(93, 353);
+        }
+        else
+        {
+            textTask.text = "Возьмите метлу";
+            image.rectTransform.sizeDelta = new Vector2(175, 45);
+            image.rectTransform.position = new Vector2(93, 393);
         }
     }
 }
