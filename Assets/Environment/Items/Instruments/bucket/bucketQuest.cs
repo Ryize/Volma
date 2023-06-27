04a54c16178ad538d1dafa8874f938db84fa885a
matvejcekasov <chekashovmatvey@gmail.com>
2023-06-27 10:25:50 +0300

    fix: Исправлена работа открытия крана

--- Assets/Environment/Items/Instruments/bucket/bucketQuest.cs
+++ Assets/Environment/Items/Instruments/bucket/bucketQuest.cs
@@ -3,13 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class bucketQuest : MonoBehaviour
 {
     public Camera playerCamera;
     public GameObject arm; // рука
+    private bool _isOpen = false;
     void Update()
     {
+        if (CameraLook().Contains("faucet") && Input.GetKeyDown(KeyCode.E) && arm.transform.childCount == 0)
+        {
+            if (!_isOpen)
+            {
+                GameObject.FindGameObjectWithTag("tapHandle").transform.Rotate(new Vector3(0f, 0f, -90f));
+                _isOpen = true;
+                GameObject.FindGameObjectWithTag("effect").GetComponent<ParticleSystem>().maxParticles = 100;
+            }
+            else
+            {
+                GameObject.FindGameObjectWithTag("tapHandle").transform.Rotate(new Vector3(0f, 0f, 90f));
+                _isOpen = false;
+                GameObject.FindGameObjectWithTag("effect").GetComponent<ParticleSystem>().maxParticles = 0;
+            }
+        }
         if (arm.transform.childCount == 0)
             return;
         
@@ -17,19 +34,13 @@ public class bucketQuest : MonoBehaviour
         GameObject obj = arm.transform.GetChild(0).GameObject();
         
         // проверка на тэг ведра без всего
-        if (obj.tag.ToLower().Contains("basket_1"))
-        {
-            if (CameraLook().Contains("faucet") && Input.GetKeyDown(KeyCode.E))
+        if (CameraLook().Contains("faucet") && Input.GetKeyDown(KeyCode.E) && _isOpen && obj.tag.ToLower().Contains("basket_1"))
             {
                 arm.GetComponent<TakeItem>().Drop();
                 obj.transform.position = new Vector3(7, 1, 99999);
-                GameObject.FindGameObjectWithTag("basket_2_item").transform.position = new Vector3(7f, 1f, -2.852f);
-                GameObject.FindGameObjectWithTag("tapHandle").transform.Rotate(new Vector3(0f, 0f, 90f));
-                // GameObject.FindGameObjectWithTag("tapHandle").transform.position = new Vector3(0f, 2.595592f, 0f);
+                GameObject.FindGameObjectWithTag("basket_2_item").transform.position = new Vector3(7f, 3f, -2.852f);
             }
         }
-        
-    }
     private string CameraLook()
     {
         RaycastHit hit;
