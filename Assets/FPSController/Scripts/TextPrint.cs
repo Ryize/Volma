a79a02f2ab250955341126a645b23ed359f55a3e
matvejcekasov <chekashovmatvey@gmail.com>
2023-06-28 12:00:44 +0300

    refactor: Переписан кусок логики для изменения текста задания

--- Assets/FPSController/Scripts/TextPrint.cs
+++ Assets/FPSController/Scripts/TextPrint.cs
@@ -6,33 +6,17 @@ using UnityEngine.UI;
 public class TextPrint : MonoBehaviour
 {
     // Поставить квест
-    public void setTask(String title, String description)
+    public void setTask(String title = "", String description = "")
     {
         var Title = GameObject.FindGameObjectsWithTag("Task")[0].GetComponent<TMP_Text>();
         var Description = GameObject.FindGameObjectsWithTag("Task")[1].GetComponent<TMP_Text>();
-
         if (title == Title.text && description == Description.text || description == "")
         {
             return;
         }
-        Title.text = title;
-        
-        int PredLengthDescription = Description.text.Length;
-        Description.text = description;
-
-        var image = GameObject.FindGameObjectWithTag("menuImage").GetComponent<Image>();
-        image.rectTransform.sizeDelta = new Vector2(325, 250 + (Description.text.Length / 15) * 30);
-        Vector2 ImagePos = image.rectTransform.position;
-        if (PredLengthDescription > Description.text.Length)
-        {
-            image.rectTransform.position = new Vector2(ImagePos.x, ImagePos.y + ((PredLengthDescription - Description.text.Length) / 15) * 15);
-        }
-        else
-        {
-            image.rectTransform.position = new Vector2(ImagePos.x, ImagePos.y - (Description.text.Length / 15) * 15);
-        } 
+        changeTitle(title);
+        changeDescription(description);
     }
-
     public void changeTitle(String title)
     {
         var Title = GameObject.FindGameObjectsWithTag("Task")[0].GetComponent<TMP_Text>();
