58a3217f1f9634ff25b1cbde74af345423829ca9
matvejcekasov <chekashovmatvey@gmail.com>
2023-06-28 11:24:01 +0300

    refactor: Убраны вспомогательные выводы, немного улучшено качество кода

--- Assets/FPSController/Scripts/TextPrint.cs
+++ Assets/FPSController/Scripts/TextPrint.cs
@@ -6,28 +6,19 @@ using UnityEngine.UI;
 public class TextPrint : MonoBehaviour
 {
     // Поставить квест
-    public void setTask(String title = "", String description = "")
+    public void setTask(String title, String description)
     {
         var Title = GameObject.FindGameObjectsWithTag("Task")[0].GetComponent<TMP_Text>();
         var Description = GameObject.FindGameObjectsWithTag("Task")[1].GetComponent<TMP_Text>();
-        Debug.Log("!!! ");
-        Debug.Log(description);
-        Debug.Log(Description.text);
+
         if (title == Title.text && description == Description.text || description == "")
         {
             return;
         }
-        if (title != "")
-        {
-            Title.text = title;
-        }
-
+        Title.text = title;
+        
         int PredLengthDescription = Description.text.Length;
-
-        if (description != "")
-        {
-            Description.text = description;
-        }
+        Description.text = description;
 
         var image = GameObject.FindGameObjectWithTag("menuImage").GetComponent<Image>();
         image.rectTransform.sizeDelta = new Vector2(325, 250 + (Description.text.Length / 15) * 30);
@@ -39,6 +30,6 @@ public class TextPrint : MonoBehaviour
         else
         {
             image.rectTransform.position = new Vector2(ImagePos.x, ImagePos.y - (Description.text.Length / 15) * 15);
-        }
-          }
+        } 
+    }
 }
