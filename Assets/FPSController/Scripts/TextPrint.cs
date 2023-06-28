0ecc25f8f2fd4ef118c2ffb32c14dd795e27e0ec
matvejcekasov <chekashovmatvey@gmail.com>
2023-06-28 11:57:08 +0300

    feat: Добавлен метод для изменения описания задания

--- Assets/FPSController/Scripts/TextPrint.cs
+++ Assets/FPSController/Scripts/TextPrint.cs
@@ -38,4 +38,24 @@ public class TextPrint : MonoBehaviour
         var Title = GameObject.FindGameObjectsWithTag("Task")[0].GetComponent<TMP_Text>();
         Title.text = title;
     }
+
+    public void changeDescription(String description)
+    {
+        var Description = GameObject.FindGameObjectsWithTag("Task")[1].GetComponent<TMP_Text>();
+        
+        int PredLengthDescription = Description.text.Length;
+        Description.text = description;
+        
+        var image = GameObject.FindGameObjectWithTag("menuImage").GetComponent<Image>();
+        image.rectTransform.sizeDelta = new Vector2(325, 250 + (Description.text.Length / 15) * 30);
+        Vector2 ImagePos = image.rectTransform.position;
+        if (PredLengthDescription > Description.text.Length)
+        {
+            image.rectTransform.position = new Vector2(ImagePos.x, ImagePos.y + ((PredLengthDescription - Description.text.Length) / 15) * 15);
+        }
+        else
+        {
+            image.rectTransform.position = new Vector2(ImagePos.x, ImagePos.y - (Description.text.Length / 15) * 15);
+        } 
+    }
 }
