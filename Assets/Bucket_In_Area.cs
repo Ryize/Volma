36b040e46c0742a06a8c541b469e37b1acae7b0d
Alexander Menko <36342234+MrFireDeN@users.noreply.github.com>
2023-06-29 15:32:49 +0300

    feat: скрипт крана НАЧАЛО

---
+++ Assets/Bucket_In_Area.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Bucket_In_Area : MonoBehaviour
+{
+    public bool isInArea = false;
+}
---
+++ Assets/Kran_Script.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kran_Script : MonoBehaviour
+{
+    public Bucket_In_Area bucket_In_Area;
+
+    void Update()
+    {
+        
+    }
+
+    void RotateKran () {
+    }
+}
