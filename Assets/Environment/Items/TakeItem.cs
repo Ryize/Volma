b43cc4bef7f4c6931f2f3f17acbf4417a6c446cf
matvejcekasov <chekashovmatvey@gmail.com>
2023-06-29 12:39:41 +0300

    !refactor: Рефакторинг

--- Assets/Environment/Items/TakeItem.cs
+++ Assets/Environment/Items/TakeItem.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 
+/// <summary>
+/// Класс для взаимодействия с предметами в игре.
+/// </summary>
 public class TakeItem : MonoBehaviour
 {
     public GameObject camera;
     public float distance = 3f;
     public GameObject currentItem;
-    public bool canToTake = false;
-    
-    void Update()
+    public bool canTake = false;
+
+    private void Update()
     {
         // На E - взять, на Q - бросить
         if (Input.GetKeyDown(KeyCode.E))
             Take();
-        if(Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q))
             Drop();
     }
 
+    /// <summary>
+    /// Взять предмет.
+    /// </summary>
     public void Take()
     {
         // Позволяет взять предмет
@@ -23,30 +29,34 @@ public class TakeItem : MonoBehaviour
 
         if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, distance))
         {
-            if(hit.transform.tag.ToLower().Contains("item"))
+            if (hit.transform.CompareTag("Item"))
             {
                 // Если предмет уже взят - бросить текущий
-                if (canToTake) 
+                if (canTake)
                     Drop();
-                
+
                 currentItem = hit.transform.gameObject;
                 currentItem.GetComponent<Rigidbody>().isKinematic = true;
                 currentItem.transform.parent = transform;
                 currentItem.layer = 6;
                 currentItem.GetComponent<TakenPosition>().Take(currentItem);
-                canToTake = true;
+                canTake = true;
                 currentItem.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
             }
         }
     }
 
+    /// <summary>
+    /// Бросить предмет.
+    /// </summary>
     public void Drop()
     {
-        // Позволяет бросить предмеи
+        // Позволяет бросить предмет
         currentItem.transform.parent = null;
         currentItem.layer = 0;
+
         // Нужно для падения под 0 углом, чтобы предметы падали ровно
-        if (currentItem.tag == "basket_1_item")
+        if (currentItem.CompareTag("basket_1_item"))
         {
             // У ведра другие базовые координаты
             currentItem.transform.eulerAngles = new Vector3(-90, 0, 0);
@@ -55,9 +65,10 @@ public class TakeItem : MonoBehaviour
         {
             currentItem.transform.eulerAngles = new Vector3(0, 0, 0);
         }
+
         currentItem.GetComponent<Rigidbody>().isKinematic = false;
         currentItem.GetComponent<TakenPosition>().Drop();
-        canToTake = false;
+        canTake = false;
         currentItem = null;
     }
 }
