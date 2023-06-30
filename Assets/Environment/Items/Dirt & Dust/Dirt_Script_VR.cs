327fd5a822a53f3d392f8b80551cc64ed3beae72
Alexander Menko <36342234+MrFireDeN@users.noreply.github.com>
2023-06-30 14:15:20 +0300

    feat: реализован скрипт метлы в VR

---
+++ Assets/Environment/Items/Dirt & Dust/Dirt_Script_VR.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dirt_Script_VR : MonoBehaviour
+{
+    public Dirt_Script ds;
+    public Rigidbody broom;
+
+    private void OnTriggerEnter(Collider other) {
+        //Debug.Log("5");
+        // Если взаимодействует не метла
+        if (!other.transform.name.ToLower().Contains("broom"))
+            return;
+        //Debug.Log("6");
+
+        float force = broom.velocity.magnitude;
+        //Debug.Log("force = " + force);
+        //Debug.Log("broom.velocity = " + broom.velocity);
+
+        // foreach (ContactPoint contact in other.contacts)
+        // {
+        //     force += contact.normalImpulse;
+        // }
+
+        ds.dirtCounter -= force;
+    }
+}
