8b8c514307f28eaa93a9cf40c4a33274b8d44a76
Alexander Menko <pods.ru@yandex.ru>
2023-11-01 16:26:43 +0300

    feat: Добавил рабочую доску с квестами(без квестов))

--- Assets/TextChange_Script.cs
+++ Assets/TextChange_Script.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class TextChange_Script : MonoBehaviour
 {
     public string newText;
     public TextMeshProUGUI currentText;
+    private List<string> Quests = new List<string>();
+    private int currentQuest = 0;
 
-   public void ChangeText()
+
+    private void Start()
     {
-        currentText.text = newText;
+        int k = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            Quests.Add(k.ToString());
+            k++;
+            Debug.Log(Quests[i]); 
+        }
+        Debug.Log(Quests);   
+    }
+    public void ChangeTextNext()
+    {
+        currentQuest++;
+        currentText.text = Quests[currentQuest];
+    }
+    public void ChangeTextPrevious()
+    {
+        if(currentQuest!=0)
+        {
+            currentQuest--;
+            currentText.text = Quests[currentQuest];
+        }
     }
 }
