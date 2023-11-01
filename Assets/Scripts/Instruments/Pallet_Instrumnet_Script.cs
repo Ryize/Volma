36925d8a2395db334185b2f21bca861ed8a7cfeb
matvejcekasov <chekashovmatvey@gmail.com>
2023-11-01 15:05:58 +0300

    refactor: Добавлена документация

--- Assets/Scripts/Instruments/Pallet_Instrumnet_Script.cs
+++ Assets/Scripts/Instruments/Pallet_Instrumnet_Script.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Pallet_Instrumnet_Script : MonoBehaviour
 {
-    //public EventZone_Script counter;
-    public Rigidbody shpatel;
-
+    /*
+     * Отвечает за нанесения клея на ПГП
+    */
     private void OnTriggerEnter(Collider other)
     {
         if (!(other.tag.ToLower().Contains("task") || other.tag.ToLower().Contains("basket")))
@@ -12,31 +12,15 @@ public class Pallet_Instrumnet_Script : MonoBehaviour
         
         string otherName = other.transform.name.ToLower();
         MeshRenderer meshRenderer = transform.gameObject.GetComponent<MeshRenderer>();
-
-        Debug.Log("Shpatel: " + meshRenderer.enabled + " name: " + otherName);
+        
         // Если на шпателе есть клей
         if (!meshRenderer.enabled) {
-            Debug.Log("shpatel1");
             // Если объект ведро, жижа замешена и на шпателе нет клея
             if (otherName.Contains("basket") && 
             other.transform.parent.GetChild(3).gameObject.activeSelf) {
                 meshRenderer.enabled = true;
-                Debug.Log("shpatel2");
             }
         }
-        // else {
-        //     // Если объект event
-        //     if (otherName.Contains("horizontalzone") || otherName.Contains("verticalzone")) {
-        //         Debug.Log("shpatel3");
-        //         EventZone_Script counter = other.transform.gameObject.GetComponent<EventZone_Script>();
-        //         counter.EventZoneCounter -= shpatel.velocity.magnitude;
-
-        //         if (counter.EventZoneCounter < 0.1) {
-        //             Destroy(other.transform.gameObject);
-        //             meshRenderer.enabled = false;
-        //         }
-        //     }
-        // }
     }
 
     private void OnTriggerExit(Collider other)
@@ -62,8 +46,5 @@ public class Pallet_Instrumnet_Script : MonoBehaviour
         if (otherName.Contains("horizontalglue") || otherName.Contains("verticalglue")) {
             Destroy(other.transform.gameObject);
         }
-        
-        /*if (eventZone.childCount <= 2)
-            eventZone.GetComponent<PGP_Event_Zone>().CompleteQuest();*/
     }
 }
