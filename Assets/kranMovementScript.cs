642c9aa529ccaedb25222078cfb8c5821b94305d
Gissigunth <sagalaevmm@gmail.com>
2023-11-07 20:29:04 +0300

    feat:Добавлены звуки и тестовые скрипты
    Добавлены звуки инструментов и тестовые скрипты для их воспроизведения

--- Assets/kranMovementScript.cs
+++ Assets/kranMovementScript.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kranMovementScript : MonoBehaviour
+public class VRBladeSpin : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip spinSound;
+    private AudioSource audioSource;
+    private float spinThreshold1 = 180f; // Пороговое значение для определения поворота
+    private float spinThreshold2 = 270f; // Пороговое значение для определения поворота
+
+    private Vector3 lastRotation;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        lastRotation = transform.eulerAngles;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float spinMagnitude = Quaternion.Angle(Quaternion.Euler(lastRotation), transform.rotation);
+
+        if (spinMagnitude > spinThreshold1)//&& spinMagnitude < spinThreshold2)
+        {
+            audioSource.clip = spinSound;
+            audioSource.Play();
+        }
+
+        lastRotation = transform.eulerAngles;
     }
 }
