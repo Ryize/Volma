5f49ce2f4b1fd0cca1ae89c821c399db9cf3ce3e
Alexander Menko <36342234+MrFireDeN@users.noreply.github.com>
2023-11-08 12:53:50 +0300

    fix: исправления после megre

--- Assets/Scripts/Instruments/Mixer_Animation_Instrument_Script.cs
+++ Assets/Scripts/Instruments/Mixer_Animation_Instrument_Script.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using Valve.VR;
 
 public class Mixer_Animation_Instrument_Script : MonoBehaviour
 {
     //переделать в приватную переменную
     public float speed = 10;
     private AudioSource mixerMovementSound;
+    private SteamVR_Action_Single buttonTrigger = SteamVR_Input.GetSingleAction("Squeeze");
     
     void Update()
     {
+        speed = buttonTrigger.axis * 10;
+        
         float x = transform.eulerAngles.x;
         float y = transform.eulerAngles.y;
         float z = transform.eulerAngles.z + speed;
