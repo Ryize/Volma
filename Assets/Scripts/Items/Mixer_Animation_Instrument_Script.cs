f3f9952c0d3962b950347b76919ba0045934de30
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2023-11-09 19:49:45 +0300

    refactor: рефакторинг иерархии
    Добавлены папки intruments, resources заменены на items и room заменена environment.

---
+++ Assets/Scripts/Items/Mixer_Animation_Instrument_Script.cs
@@ -0,0 +1,62 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Mixer_Animation_Instrument_Script : MonoBehaviour
+{
+    private float _speed;
+    private AudioSource _mixerMovementSound;
+    private SteamVR_Action_Single _buttonTrigger = 
+        SteamVR_Input.GetAction<SteamVR_Action_Single>("buggy", "Throttle");
+    //SteamVR_Input.GetSingleAction("Throttle");
+    private Interactable _interactable;
+    private Transform _auger;
+    private float _augerX, _augerY, _augerZ;
+
+    private void Start()
+    {
+        _auger = transform.GetChild(2);
+        _interactable = GetComponent<Interactable>();
+        _mixerMovementSound = GetComponentInParent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (_interactable.attachedToHand)
+        {
+            SteamVR_Input_Sources hand = _interactable.attachedToHand.handType;
+            
+            Debug.Log("[Mixer_Animation_Instrument_Script] Mixer in hand");
+            Debug.Log("[Mixer_Animation_Instrument_Script] _buttonTrigger = " + _buttonTrigger.GetAxis(hand));
+            _speed = _buttonTrigger.axis * 10;
+        
+            _augerX = _auger.eulerAngles.x;
+            _augerY = _auger.eulerAngles.y;
+            _augerZ = _auger.eulerAngles.z + _speed;
+            _auger.eulerAngles = new Vector3(_augerX, _augerY, _augerZ);
+        }
+        else
+        {
+            _speed = 0f;
+        }
+        
+        if(_speed > 0f && !_mixerMovementSound.isPlaying)
+        {
+            
+            _mixerMovementSound.Play();
+        }
+
+        if(_speed <= 0f && _mixerMovementSound.isPlaying)
+        {
+            
+            _mixerMovementSound.Pause();
+        }
+    }
+
+    public float GetSpeed()
+    {
+        return _speed;
+    }
+}
