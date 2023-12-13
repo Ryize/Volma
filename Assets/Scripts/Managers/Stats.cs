48e0d9a365d54258bf2a8de0f608759ff0812622
Alexander Menko <36342234+MrFireDeN@users.noreply.github.com>
2023-12-13 16:28:23 +0300

    feat: добавлена статистика для воды

--- Assets/Scripts/Managers/Stats.cs
+++ Assets/Scripts/Managers/Stats.cs
@@ -35,12 +35,34 @@ public class Stats : Repository
              * При изменении кол-ва цемента сразу меняем и данные в статистике.
              *
              * Цемент должен идти с кг и разделяться тире (переменная dash).
-            */
+             */
             _cement = (float) Math.Round(value, 2);
             
             List<string> statsEl = text.currentText.text.Split(dash).ToList();
             List<string> cementList = statsEl[0].Replace("кг", "").Split(" ").ToList();
             text.currentText.text = cementList[0] + " " + _cement + "кг" + "\n" + dash + statsEl[1];
+        }
+    }
+    
+    public float water
+    {
+        get
+        {
+            return _water;
+        }
+        set
+        {
+            /*
+             * При изменении кол-ва воды сразу меняем и данные в статистике.
+             *
+             * Цемент должен идти с кг и разделяться тире (переменная dash).
+             * Вода должна идти с л и разделяться тире (переменная dash).
+             */
+            _water = (float) Math.Round(value, 2);
+            
+            List<string> statsEl = text.currentText.text.Split(dash).ToList();
+            List<string> waterList = statsEl[1].Replace("л", "").Split(" ").ToList();
+            text.currentText.text = statsEl[0] + dash + waterList[0] + " " + _water + "л";
         }
     }
 }
