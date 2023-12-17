50467b3297b192d1663d114477ad96d5e8a9245a
rokosvlg <130643419+rokosvlg@users.noreply.github.com>
2023-12-17 21:57:56 +0300

    feat: полностью рабочее радио(требует настройки))

--- Assets/Scripts/ChannelSwitchScript.cs
+++ Assets/Scripts/ChannelSwitchScript.cs
@@ -4,77 +4,130 @@ using UnityEngine;
 
 public class ChannelSwitchScript : MonoBehaviour
 {
+    /*
+     * Класс реализующий смену треков на радио
+     * Ссылка на гайд если не понятно(https://www.youtube.com/watch?v=EHKrMWGEZPU)
+     *
+     *
+     * Для того что бы создать трек нужно зайти в папку ScriptableObjects(Assets), нажать ПКМ и в верху выбрать пункт Create.
+     * После чего выбрать пункт RadioTrackScript.
+     * В создавшийся объект нужно засунуть трек, пункт TrackAudioClip, предварительно закинутый в папку Audio,
+     * после чего выбрать крутилку радио и данный скрипт.
+     * В свойствах скрипта будет масиив AudioTracks.
+     * Нужно будет выбрать количество элементов в масииве и добавить трек в нужную нам позицию, после чего в скрипте прописать логику(по аналогии с остальными). 
+     */
+    //Заголовок(хз в гайде так делали)
     [Header("List of Tracks")] 
+    // Список с треками
     [SerializeField] private RadioTrackScript[] audioTracks;
 
+    //Индекс текущего трека
     private int trackIndex;
-    
+    // Текущее значение поворота крутилки
     private float _lastRotation;
+    //Предыдущее значение поворота крутилки
+    private float _prevRotation;
+    //Аудиосоурс который воспроизводит треки
     private AudioSource radioAudioSource;
+    /*
+     * Методы со стартовыми данными
+     *
+     */
     void Start()
     {
+        // Не особо шарю так сделали в гайде
         radioAudioSource = GetComponent<AudioSource>();
+        //Значение трека поставлено на -1 т.к. 0 занят первым треком в списке
         trackIndex = -1;
-        radioAudioSource.clip = audioTracks[trackIndex].trackAudioClip;
+        //Получаем изначальный угол поворота
+        _prevRotation=transform.localRotation.eulerAngles.y;
     }
-    
+    /*
+     * Метод обновления трека и их запуск
+     */
     public void UpdateTrack(int index)
     {
-        if (index != -1)
-        {
             radioAudioSource.clip = audioTracks[index].trackAudioClip;
-        }
+            PlayAudio();
     }
+    /*
+     * Метод запуска трека
+     */
     public void PlayAudio()
     {
         radioAudioSource.Play();
     }
-
-    public void StopAudio()
+    /*
+     * Метод остановки трека
+     */
+    public void StopAudio() 
     {
         radioAudioSource.Stop();
     }
-    
-    // Update is called once per frame
+    /*
+     * Основной метод который следит за поворотом ручки
+     * Сделал всё в нём а не в отдельном методе потому что лень
+     */
     void Update()
-    {
-        //float spinMagnitude = Quaternion.Angle(Quaternion.Euler(_lastRotation), transform.rotation);
+    {   // Получение актуального значения поворота
         _lastRotation = transform.localRotation.eulerAngles.y;
-        if (_lastRotation >= 30 && _lastRotation <= 60)
+        //Проверяем изменилось ли значение поворота
+        if (_lastRotation != _prevRotation)
         {
-            UpdateTrack(1);
-            Debug.Log("1");
-            PlayAudio();
-        }
-        else if (_lastRotation >= 120 && _lastRotation <= 150)
-        {
-            UpdateTrack(2);
-            Debug.Log("2");
-            PlayAudio();
-        }
-        else if (_lastRotation >= 210 && _lastRotation <= 240)
-        {
-            UpdateTrack(3);
-            Debug.Log("3");
-            PlayAudio();
-        }
-        else if (_lastRotation >= 300 && _lastRotation <= 330)
-        {
-            UpdateTrack(4);
-            Debug.Log("4");
-            PlayAudio();
-        }
-        else if (_lastRotation>=0 && _lastRotation<=15)
-        {
-            StopAudio();
-            Debug.Log("стоп");
-        }
-        else
-        {
-            UpdateTrack(0);
-            PlayAudio();
-            Debug.Log("шум");
-        }
+            //Запоминаем новые значения поворота
+            _prevRotation = _lastRotation;
+            //1й трек и его диапазон
+            if (_lastRotation >= 30f && _lastRotation <= 60f)
+            {
+                if (trackIndex != 1)
+                {
+                    trackIndex = 1;
+                    UpdateTrack(trackIndex);
+                }
+            }
+            //2й трек и его диапазон
+            else if (_lastRotation >= 120f && _lastRotation <= 150f )
+            {
+                if (trackIndex != 2)
+                {
+                    trackIndex = 2;
+                    UpdateTrack(trackIndex);
+                }
+            }
+            //3й трек и его диапазон
+            else if (_lastRotation >= 210f && _lastRotation <= 240f && trackIndex!=3)
+            {
+                if (trackIndex != 3)
+                {
+                    trackIndex = 3;
+                    UpdateTrack(trackIndex);
+                }
+            }
+            //4й трек и его диапазон
+            else if (_lastRotation >= 300f && _lastRotation <= 330f && trackIndex!=4)
+            {
+                if (trackIndex != 4)
+                {
+                    trackIndex = 4;
+                    UpdateTrack(trackIndex);
+                }
+            }
+            //Остановка Аудио
+            else if (_lastRotation >= 0f && _lastRotation <= 5f)
+            {
+                StopAudio();
+            }
+            // Во всех других случаях мы ставим помехи(типа реалистично)
+            else
+            {
+                if (trackIndex != 0)
+                {
+                    trackIndex = 0;
+                    UpdateTrack(trackIndex); 
+                }
+            }
 
+        }
+        
     }
 }
--- Assets/Scripts/RadioTrackScript.cs
+++ Assets/Scripts/RadioTrackScript.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//создание объекта в папке ScriptableObjects
 [CreateAssetMenu(fileName ="RadioTrackScript")]
+/*
+ * Создаём объект для хранения в себе трека
+ */
 public class RadioTrackScript : ScriptableObject
 {
     public AudioClip trackAudioClip;
