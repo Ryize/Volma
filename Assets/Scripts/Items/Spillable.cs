20b00d5494cc6960b6d435c6bd50d2afd42b8164
MrFireDeN <36342234+MrFireDeN@users.noreply.github.com>
2024-05-30 10:10:23 +0300

    feat: добавление компонента Spillable для предметов

---
+++ Assets/Scripts/Items/Spillable.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Spillable : MonoBehaviour
+{
+    /*
+     * Класс-компонент отвечающий расссыпание(выливание).
+    */
+
+    private bool _isSpilling;
+    private Vector3 lastRotation;
+
+    void Start()
+    {
+        _isSpilling = false;
+        lastRotation = transform.eulerAngles;
+    }
+
+    public bool IsSpilling()
+    {
+        if (lastRotation != transform.eulerAngles)
+        {
+            lastRotation = transform.eulerAngles;
+
+            float cosX = Mathf.Cos(transform.rotation.eulerAngles.x * Mathf.Deg2Rad);
+            float cosZ = Mathf.Cos(transform.rotation.eulerAngles.z * Mathf.Deg2Rad);
+
+            if (cosX * cosZ <= -0.1f)
+            {
+                _isSpilling |= true;
+            }
+            else
+            {
+                _isSpilling |= false;
+            }
+        }
+
+        return _isSpilling;
+    }
+}
